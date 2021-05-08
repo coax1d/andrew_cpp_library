5fbf86e013d6ef404d1634fc1ac4a897bfa9ebd8
Andrew Burger <andrewburger1991@gmail.com>
2021-05-08 02:38:36 -0700

    Using shared_ptr

--- Queue/Queue.hpp
+++ Queue/Queue.hpp
@@ -5,20 +5,21 @@ Author: Andrew Burger */
 #define QUEUE_QUEUE_HPP_
 #include <iostream>
 #include <string>
+#include <memory>
 #include <assert.h>
 #include <cstring>
 
 template<class T>
 struct ListNode {
 
-    ListNode(ListNode *next) : next_{next} {}
+    explicit ListNode(ListNode *next) : next_{next} {}
     ListNode(T &value) : value_{std::move(value)} {}
     ListNode(T &value, ListNode *next) :
         value_{std::move(value)},
         next_{next} {}
 
     T value_;
-    ListNode *next_;
+    std::shared_ptr<ListNode<T>> next_;
 };
 
 template<class T>
@@ -36,34 +37,26 @@ class Queue {
         T get_queue();
 
     private:
-        ListNode<T> *front_queue_;
-        ListNode<T> *back_queue_;
+        std::shared_ptr<ListNode<T>> front_queue_;
+        std::shared_ptr<ListNode<T>> back_queue_;
         size_t queue_len_ = 0;
 };
 
 template<class T>
 Queue<T>::Queue() {
 
-    front_queue_ = new ListNode<T>(nullptr);
-    back_queue_ = new ListNode<T>(nullptr);
+    front_queue_ = std::make_shared<ListNode<T>>(nullptr);
+    back_queue_ = std::make_shared<ListNode<T>>(nullptr);
     front_queue_->next_ = back_queue_;
 }
 
 template<class T>
-Queue<T>::~Queue() {
-
-    auto current = front_queue_;
-    while (current) {
-        auto temp = current;
-        current = current->next_;
-        delete temp;
-    }
-}
+Queue<T>::~Queue() {}
 
 template<class T>
 void Queue<T>::put_queue(T &item) {
 
-    auto new_node = new ListNode<T>(item);
+    auto new_node = std::make_shared<ListNode<T>>(item);
 
     auto temp = front_queue_->next_;
     front_queue_->next_ = new_node;
@@ -74,7 +67,7 @@ void Queue<T>::put_queue(T &item) {
 template<class T>
 void Queue<T>::put_queue(T &&item) {
 
-    auto new_node = new ListNode<T>(item);
+    auto new_node = std::make_shared<ListNode<T>>(item);
 
     auto temp = front_queue_->next_;
     front_queue_->next_ = new_node;
@@ -103,7 +96,6 @@ T Queue<T>::get_queue() {
         current->next_ = current->next_->next_;
 
         return_val = removed_node->value_;
-        delete removed_node;
         queue_len_--;
     }
 
