f745e1976ac04589ee59c9577783ae86f19f0079
coax1d <andrewburger1991@gmail.com>
2021-05-11 15:21:08 -0700

    Is this use of auto appropriate? (#2)
    * Is this use of auto appropriate?
    
    * deleting copy constructor/copy assign
    
    * fixed deletion
    
    * whoops
    
    * Whoops final
    
    * Using shared_ptr
    
    * Cleanup changes and universal reference use
    
    * Adding makefile
    
    * Removed heap allocations for front/back

--- Queue/Queue.hpp
+++ Queue/Queue.hpp
@@ -5,20 +5,21 @@ Author: Andrew Burger */
 #define QUEUE_QUEUE_HPP_
 #include <iostream>
 #include <string>
+#include <memory>
+#include <utility>
 #include <assert.h>
 #include <cstring>
 
 template<class T>
 struct ListNode {
 
-    ListNode(ListNode *next) : next_{next} {}
-    ListNode(T &value) : value_{std::move(value)} {}
-    ListNode(T &value, ListNode *next) :
-        value_{std::move(value)},
+    ListNode() = default;
+    explicit ListNode(T &&value, std::shared_ptr<ListNode<T>> next = nullptr) :
+        value_{value},
         next_{next} {}
 
-    T value_;
-    ListNode *next_;
+    T value_{};
+    std::shared_ptr<ListNode<T>> next_{nullptr};
 };
 
 template<class T>
@@ -28,59 +29,41 @@ class Queue {
         Queue<T>();
         ~Queue<T>();
 
-        void put_queue(T &item);
-        void put_queue(T &&item);
+        Queue<T>(Queue<T> &) = delete;
+        Queue<T> & operator=(Queue<T> &) = delete;
+
+        template<class U>
+        void put_queue(U &&item);
+
         T get_queue();
 
     private:
-        ListNode<T> *front_queue_;
-        ListNode<T> *back_queue_;
-        size_t queue_len_ = 0;
+        std::shared_ptr<ListNode<T>> front_queue_{nullptr};
+        std::shared_ptr<ListNode<T>> back_queue_{nullptr};
+        size_t queue_len_{};
 };
 
 template<class T>
-Queue<T>::Queue() {
-
-    front_queue_ = new ListNode<T>(nullptr);
-    back_queue_ = new ListNode<T>(nullptr);
-    front_queue_->next_ = back_queue_;
-}
+Queue<T>::Queue() {}
 
 template<class T>
-Queue<T>::~Queue() {
+Queue<T>::~Queue() {}
 
-    ListNode<T> *current = front_queue_;
-    while (current) {
-        ListNode<T> *temp = current;
-        current = current->next_;
-        delete temp;
+template<class T>
+template<class U>
+void Queue<T>::put_queue(U &&item) {
+
+    auto new_node = std::make_shared<ListNode<T>>(std::forward<T>(item));
+
+    auto temp = front_queue_;
+    front_queue_ = new_node;
+    new_node->next_ = temp;
+
+    if (!new_node->next_) {
+        back_queue_ = new_node;
     }
-}
 
-template<class T>
-void Queue<T>::put_queue(T &item) {
-
-    ListNode<T> *new_node = new ListNode<T>(item);
-
-    ListNode<T> *temp = front_queue_->next_;
-    front_queue_->next_ = new_node;
-    new_node->next_ = temp;
     queue_len_++;
-
-    std::cout << "Added item to Linked List! value is " << new_node->value_ << std::endl;
-}
-
-template<class T>
-void Queue<T>::put_queue(T &&item) {
-
-    ListNode<T> *new_node = new ListNode<T>(item);
-
-    ListNode<T> *temp = front_queue_->next_;
-    front_queue_->next_ = new_node;
-    new_node->next_ = temp;
-    queue_len_++;
-
-    std::cout << "Added item to Linked List! value is " << new_node->value_ << std::endl;
 }
 
 template<class T>
@@ -90,21 +73,27 @@ T Queue<T>::get_queue() {
 
     T return_val;
 
-    ListNode<T> *current = front_queue_;
+    if (front_queue_ == back_queue_) { // One node in the list
+        return_val = front_queue_->value_;
+        front_queue_ = nullptr;
+        back_queue_ = nullptr;
+        return return_val;
+    }
+
+    auto current = front_queue_;
     while (current != back_queue_) {
 
         // Need to get to the ListNode which is two nodes
         // prior to the back_queue_ ListNode
-        if (current->next_->next_ != back_queue_) {
+        if (current->next_ != back_queue_) {
             current = current->next_;
             continue;
         }
 
-        ListNode<T> *removed_node = current->next_;
-        current->next_ = current->next_->next_;
+        back_queue_ = current;
 
+        auto removed_node = current->next_;
         return_val = removed_node->value_;
-        delete removed_node;
         queue_len_--;
     }
 
--- Queue/test.cpp
+++ Queue/test.cpp
@@ -2,26 +2,30 @@
 #include <string>
 #include "Queue.hpp"
 
+template<class T>
+void print(const std::string &msg, const T &value) {
+    std::cout << value << msg << std::endl;
+}
 
 void test() {
+
     Queue<int> queue;
 
     int test1 = 1;
     int test2 = 2;
+    std::string put_queue_msg(" Put in the queue");
+    std::string get_queue_msg(" Retrieved from the queue");
+
+    // put_queue test
     queue.put_queue(1);
     queue.put_queue(test2);
+    print(put_queue_msg, test1);
+    print(put_queue_msg, test2);
 
-    int test3 = queue.get_queue();
-
-    std::cout << "Got " << test3 << " from the queue" << std::endl;
-    int test4 = queue.get_queue();
-    std::cout << "Got " << test4 << " from the queue" << std::endl;
+    get_queue test
+    print(get_queue_msg, queue.get_queue());
+    print(get_queue_msg, queue.get_queue());
 }
-
-void print(const std::string &msg) {
-    std::cout << msg << std::endl;
-}
-
 
 int main(void) {
 
